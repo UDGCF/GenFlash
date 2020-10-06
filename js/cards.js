f9a44b0dc0be748d5a241d788c55ecc174b060b4
Arkreuz <Developer@arkreuz.com>
2020-10-06 18:17:50 -0500

    Flash

--- js/cards.js
+++ js/cards.js
@@ -3,47 +3,47 @@ flashcardDeck = {
   catchPhrase: "Test how well you know JavaScript",
   cards: [
     {
-        question: "¿Que es un intrón?",
+        question: "¿Qué es un intrón?",
         answer: "Parte del gen que no codifica ningún aminoácido.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Intron"
     },
        {
-        question: "¿Que es un Autosoma?",
+        question: "¿Qué es un Autosoma?",
         answer: "Cualquier de los cromosomas, excepto los cromosomas sexuales.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Autosoma"
     },
     {
-        question: "¿Que es Bioinformática?",
+        question: "¿Qué es Bioinformática?",
         answer: "Subdisciplina de la biología y las ciencias computacionales que se encarga de adquirir, almacenar, analizar y diseminar la información biológica, en gran parte correspondiente a las secuencias de ADN y aminoácidos.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Bioinformatica"
     },
         {
-        question: "¿Que es Cariotipo?",
+        question: "¿Qué es Cariotipo?",
         answer: "Colección de cromosomas de un individuo.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Cariotipo"
     },
         {
-        question: "¿Que es Centrómero?",
+        question: "¿Qué es Centrómero?",
         answer: "Región estrecha de un cromosoma que lo separa en un brazo corto (p) y un brazo largo (q).",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Centromero"
     },
         {
-        question: "¿Que es ​Citogenética?",
+        question: "¿Qué es ​Citogenética?",
         answer: "Campo de la genética que comprende el estudio de los cromosomas.",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Citogenetica"
     },
         {
-        question: "¿Que es Codón?",
+        question: "¿Qué es Codón?",
         answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
     },
          {
-        question: "¿Que es Codón de parada?",
+        question: "¿Qué es Codón de parada?",
         answer: "Secuencia de trinucleótidos de la molecula del ARN mensajero (ARNm) que señala un alto en la síntesis de proteínas.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon-de-parada"
@@ -55,25 +55,25 @@ flashcardDeck = {
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Congenito"
     },
          {
-        question: "¿Que es Cromosoma?",
+        question: "¿Qué es Cromosoma?",
         answer: "Paquete ordenado de ADN que se encuentra en el núcleo de la célula",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Cromosoma"
     },
          {
-        question: "¿Que es Codón?",
+        question: "¿Qué es Codón?",
         answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
     },
          {
-        question: "¿Que es un Cromosoma sexual?",
+        question: "¿Qué es un Cromosoma sexual?",
         answer: "Tipo de cromosoma que participa en la determinación del sexo.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Cromosoma-sexual"
     },
          {
-        question: "¿Que es un Cromosoma X?",
+        question: "¿Qué es un Cromosoma X?",
         answer: "Uno de los dos cromosomas sexuales.Las hembras tienen dos cromosomas X en sus células somaticas, Todos los óvulos, sin embargo, contienen solo un cromosoma X, mientras que los espermatozoides pueden contener un cromosoma X o uno Y.",
         source: "National Human Genome Research Institute",
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Cromosoma-X"
@@ -85,46 +85,163 @@ flashcardDeck = {
         sourceUrl: "https://www.genome.gov/es/genetics-glossary/Cromosoma-Y"
     },
          {
-        question: "¿Que es Codón?",
-        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
-        source: "National Human Genome Research Institute",
-        sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
+        question: "¿Qué es un Defecto congénito?",
+        answer: "Es una anormalia que se presenta desde el nacimiento.",
     },
-         {
-        question: "¿Que es Codón?",
-        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
-        source: "National Human Genome Research Institute",
-        sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
+    {
+        question: "¿Qué es una Deleción?",
+        answer: "Tipo de mutación genética en la cual se pierde material genético.",
     },
-         {
-        question: "¿Que es Codón?",
-        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
-        source: "National Human Genome Research Institute",
-        sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
+    {
+        question: "¿Diploide?",
+        answer: "Célula u organismo que tiene cromosomas emparejados, uno de cada progenitor.",
     },
-         {
-        question: "¿Que es Codón?",
-        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
-        source: "National Human Genome Research Institute",
-        sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
+    {
+        question: "¿Qué es un gen Dominante?",
+        answer: "Si los alelos de un gen son diferentes, el alelo que se expresa es el gen dominante.",
     },
-         {
-        question: "¿Que es Codón?",
-        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
-        source: "National Human Genome Research Institute",
-        sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
+    {
+        question: "¿Qué es una Duplicación?",
+        answer: "Tipo de mutación que implica la producción de una o más copias de un gen o región de un cromosoma.",
     },
-         {
-        question: "¿Que es Codón?",
-        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
-        source: "National Human Genome Research Institute",
-        sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
+    {
+        question: "¿A que se refiere Epigenética?",
+        answer: "Campo emergente de la ciencia que estudia los cambios hereditarios causados por la activación y desactivación de los genes sin ningún cambio en la secuencia de ADN subyacente del organismo.",
     },
-         {
-        question: "¿Que es Codón?",
-        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
-        source: "National Human Genome Research Institute",
-        sourceUrl: "https://www.genome.gov/es/genetics-glossary/Codon"
+    {
+        question: "¿​Qué es Epigenoma?",
+        answer: "compuestos químicos que modifican, o marcan, el genoma de manera que le dice qué hacer, dónde hacerlo y cuándo hacerlo.",
     },
+    {
+        question: "¿Qué es Exón?",
+        answer: "Porción de gen que codifica aminoácidos.",
+    },
+    {
+        question: "¿Qué es la Expresión génica?",
+        answer: "Es el proceso mediante el cual la información codificada en un gen se utiliza para dirigir el montaje de una molécula de proteína.",
+    },
+    {
+        question: "¿Qué es Fenotipo?",
+        answer: "Los rasgos observables de un individuo",
+    },
+    {
+        question: "¿Qué es un Gen?",
+        answer: "Unidad física básica de la herencia.Se transmiten de los padres a la descendencia y contienen la información necesaria para precisar sus rasgos.",
+    },
+    {
+        question: "¿Qué es un Genoma?",
+        answer: "Conjunto de instrucciones genéticas que se encuentra en una célula.",
+    },{
+        question: "¿​Qué es la Genómica?",
+        answer: "Estudio del genoma completo de un organismo.",
+    },{
+        question: "¿Qué es un Genotipo?",
+        answer: "Colección de genes de un individuo",
+    },{
+        question: "¿Qué es Haploide?",
+        answer: "Célula u organismo con un único conjunto de cromosomas. En los seres humanos, sólo los óvulos y los espermatozoides son haploides.",
+    },{
+        question: "¿Qué es un ​Haplotipo?",
+        answer: "Conjunto de variaciones del ADN, o polimorfismos, que tienden a ser heredados juntos.",
+    },{
+        question: "¿Qué es ​HapMap?",
+        answer: "Mapa de haplotipos. Es un proyecto internacional que busca relacionar variaciones en las secuencias de ADN humano con genes asociados con la salud.",
+    },{
+        question: "¿Qué es un rasgo Hereditario?",
+        answer: "Aquel que está determinado genéticamente, se transmiten de padres a hijos según las reglas de la genética mendeliana.",
+    },{
+        question: "¿Qué es la Herencia mendeliana?",
+        answer: "Los patrones de herencia que son característicos de los organismos que se reproducen sexualmente.",
+    },{
+        question: "¿Qué es Histona?",
+        answer: "Proteína que proporciona soporte estructural a un cromosoma.",
+    },{
+        question: "¿Qué es una Huella genética?",
+        answer: "Técnica de laboratorio utilizada para establecer un vínculo entre la evidencia biológica y un sospechoso en una investigación criminal.",
+    },{
+        question: "¿Qué es Homocigoto?",
+        answer: "Se refiere a haber heredado dos formas idénticas	 de un gen en particular, una de cada progenitor",
+    },{
+        question: "¿Qué es Heterocigoto?",
+        answer: "Se refiere a haber heredado dos formas diferentes de un gen en particular, una de cada progenitor.",
+    },{
+        question: "¿Qué es una Inserción?",
+        answer: "Tipo de mutación que implica la adición de material genético.",
+    },{
+        question: "¿Qué es Locus?",
+        answer: "Lugar específico del cromosoma donde está localizado un gen u otra secuencia de ADN, como su dirección genética. El plural de locus es 'loci'.",
+    },{
+        question: "¿Qué es un ​Mapa genético?",
+        answer: "Tipo de mapa cromosómico que muestra la ubicación relativa de los genes y otras características importantes. ",
+    },{
+        question: "¿Qué es un Marcador genético?",
+        answer: "Segmento de ADN con una ubicación física conocida en un cromosoma, pueden ayudar a vincular una enfermedad hereditaria con el gen responsable.",
+    },{
+        question: "¿Quién fue Mendel?",
+        answer: "Monje austriaco que en el siglo XIX estableció las leyes básicas de la herencia genética mucho antes de que el término ¨gen¨ fuera acuñado.",
+    },{
+        question: "¿Qué es un Mutágeno?",
+        answer: "Agente químico o físico, que promueve los errores en la replicación del ADN. ",
+    },{
+        question: "¿Qué es un ​Nucleótido?",
+        answer: "Pieza básica de los ácidos nucleicos, está formado por una molécula de azúcar (ribosa en el ARN o desoxirribosa en el ADN) unido a un grupo fosfato y una base nitrogenada.",
+    },{
+        question: "¿Qué es un ​Polimorfismo?",
+        answer: "Una de dos o más variantes de una secuencia particular de ADN.",
+    },{
+        question: "¿Qué es el Proyecto Genoma Humano?",
+        answer: "Proyecto internacional que mapeó y secuenció todos los genes humanos.",
+    },{
+        question: "¿Qué es un Polimorfismo de nucleótido único (SNPs)?",
+        answer: "Tipo de polimorfismo que producen una variación en un solo par de bases, se correlacionan con enfermedades, con la respuesta de los fármacos, y con otros fenotipos.",
+    },{
+        question: "¿Qué es Recesivo?",
+        answer: "Alelo que queda enmascarado cuando se transmiten alelos diferentes.",
+    },{
+        question: "¿Qué es un Microsatélite?",
+        answer: "Cecuencias cortas repetitivas de ADN, compuestas de ADN no codificante y nos son parte de ningún gen.",
+    },{
+        question: "¿Qué es un Represor?",
+        answer: "Es una proteína que desactiva la expresión de uno o más genes.",
+    },{
+        question: "¿Qué es un Síndrome?",
+        answer: "Conjunto de rasgos reconocibles o anomalías que tienden a presentarse juntos y están asociados con una enfermedad específica.",
+    },{
+        question: "¿Qué es el ​Síndrome de Down?",
+        answer: "Enfermedad genética que es consecuencia de una anomalía cromosómica. Una persona con síndrome de Down hereda todo o parte de una copia extra del cromosoma 21.",
+    },{
+        question: "¿Qué es Sustitución?",
+        answer: "Tipo de mutación en el que se sustituye un par de bases por un par de bases diferentes.",
+    },{
+        question: "¿Qué es un Telómero?",
+        answer: "El extremo de un cromosoma, son secuencias repetitivas de ADN no codificante del cromosoma que protegen de cualquier daño.",
+    },{
+        question: "¿Qué es la Transcripción?",
+        answer: "Es el proceso por el cual se genera una copia de RNA a partir la secuencia de un gene.",
+    },{
+        question: "¿Qué es Transgénico?",
+        answer: " Significa que una o más secuencias de ADN de otra especie han sido introducidos por medios artificiales.",
+    },{
+        question: "¿Qué es la Translocación?",
+        answer: "Tipo de anomalía cromosómica en la que un cromosoma se rompe y una parte de ella vuelve a unir a un cromosoma diferente.",
+    },{
+        question: "¿Qué es la Traducción?",
+        answer: "Proceso de traducir la secuencia de una molécula de ARN mensajero (ARNm) a una secuencia de aminoácidos durante síntesis de proteínas.",
+    },{
+        question: "¿Qué es la Variación en el número de copias (VCN)?",
+        answer: "Es cuando un número de copias de un gen particular cambia de un individuo a otro.",
+    },{
+        question: "¿Qué es un Alelo?",
+        answer: "Cada una de las dos o más versiones de un gen.",
+    },{
+        question: "¿Qué es Codón?",
+        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
+    },{
+        question: "¿Qué es Codón?",
+        answer: "Secuencia de tres nucleótidos de ADN o ARN que corresponde a un aminoácido específico.",
+    },
+        
+        
+        
   ]
 };
